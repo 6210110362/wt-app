babd29ee86a46296a9ff4150454665feaf4eddfe
6210110362 <6210110362@psu.ac.th>
2021-07-17 00:33:57 +0700

    section 6

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+//import { StatusBar } from 'expo-status-bar';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet,  View } from 'react-native';
-import Weather from './components/Weather';
-import Constant from 'expo-constants';
+import WeatherScreen from './screen/WeatherScreen';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+
+
+
+//import { StyleSheet,  View } from 'react-native';
+//import Weather from './components/Weather';
+//import Constant from 'expo-constants';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen}/>
+        <Stack.Screen name="Weather" component={WeatherScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+     /*<View style={styles.container}>
       <Weather  zipCode="90110" />
       <StatusBar style="auto" />
-    </View>
+     </View>*/
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
       paddingTop: Constant.statusBarHeight,
   }
-});
+});*/
